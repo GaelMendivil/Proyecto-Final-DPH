a4231661829e481b19cb6c36b1f8c468b2611435
alanloom <alanisaac.lopez@cetys.edu.mx>
2025-10-17 19:19:06 -0700

    Botones arreglados

--- WhosThatPokemon/WhosThatPokemon/Controllers/GamesController.cs
+++ WhosThatPokemon/WhosThatPokemon/Controllers/GamesController.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using Microsoft.AspNetCore.Mvc;
 
 namespace WhosThatPokemon.Controllers
@@ -20,3 +21,27 @@ namespace WhosThatPokemon.Controllers
         }
     }
 }
+=======
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhosThatPokemon.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Classic()
+        {
+            return View(); 
+        }
+
+        public IActionResult Pokedex()
+        {
+            return View(); 
+        }
+    }
+}
+>>>>>>> Stashed changes
