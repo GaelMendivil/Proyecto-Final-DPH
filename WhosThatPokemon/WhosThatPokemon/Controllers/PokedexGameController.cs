efb0469c1b80c2299bd83c09d235ea565d334b4d
alanloom <alanisaac.lopez@cetys.edu.mx>
2025-11-19 02:45:42 -0800

    ZoomGame

--- WhosThatPokemon/WhosThatPokemon/Controllers/PokedexGameController.cs
+++ WhosThatPokemon/WhosThatPokemon/Controllers/PokedexGameController.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace WhosThatPokemon.Controllers
 {
@@ -36,6 +37,35 @@ namespace WhosThatPokemon.Controllers
             return View("~/Views/PokedexGame/Index.cshtml");
         }
 
+// Autocomplete
+[HttpGet]
+[Route("/PokedexGame/SearchPokemon")]
+public async Task<IActionResult> SearchPokemon(string term)
+{
+    if (string.IsNullOrEmpty(term) || term.Length < 2)
+        return Json(new List<object>());
+
+    string url = "https://pokeapi.co/api/v2/pokemon?limit=2000";
+
+    var response = await _httpClient.GetStringAsync(url);
+    var json = JObject.Parse(response);
+
+    var results = json["results"]
+        .Where(p => p["name"].ToString()
+        .IndexOf(term, StringComparison.OrdinalIgnoreCase) >= 0)
+        .Select(p => new {
+            name = p["name"].ToString(),
+            image = $"https://img.pokemondb.net/sprites/home/normal/{p["name"]}.png"
+        })
+        .Take(10)
+        .ToList();
+
+    return Json(results);
+}
+
+
+
+//Adivinar Pokémon
         [HttpPost]
         [Route("/Pokedex/Guess")]
         public IActionResult Guess(string guess)
@@ -85,7 +115,6 @@ namespace WhosThatPokemon.Controllers
             return View("~/Views/PokedexGame/Index.cshtml");
         }
 
-
         private async Task GetRandomPokemon()
         {
             var random = new Random();
@@ -113,4 +142,5 @@ namespace WhosThatPokemon.Controllers
             attempts = 0;
         }
     }
+
 }
