445c1e55c951c7d25b8e9e25baf0a39fc0f175df
priscilanietob <165713755+priscilanietob@users.noreply.github.com>
2025-11-19 20:12:38 -0800

    PokedexGame
    juego terminado

--- WhosThatPokemon/WhosThatPokemon/Controllers/PokedexGameController.cs
+++ WhosThatPokemon/WhosThatPokemon/Controllers/PokedexGameController.cs
@@ -45,27 +45,44 @@ public async Task<IActionResult> SearchPokemon(string term)
     if (string.IsNullOrEmpty(term) || term.Length < 2)
         return Json(new List<object>());
 
-    string url = "https://pokeapi.co/api/v2/pokemon?limit=2000";
-
+    string url = "https://pokeapi.co/api/v2/pokemon?limit=1025";
     var response = await _httpClient.GetStringAsync(url);
     var json = JObject.Parse(response);
 
-    var results = json["results"]
+    var filtered = json["results"]
         .Where(p => p["name"].ToString()
         .IndexOf(term, StringComparison.OrdinalIgnoreCase) >= 0)
-        .Select(p => new {
-            name = p["name"].ToString(),
-            image = $"https://img.pokemondb.net/sprites/home/normal/{p["name"]}.png"
-        })
         .Take(10)
         .ToList();
+
+    var results = new List<object>();
+
+    foreach (var p in filtered)
+    {
+        string name = p["name"].ToString();
+
+        string pokeUrl = p["url"].ToString();   
+        string id = pokeUrl.TrimEnd('/').Split('/').Last();
+
+        string spriteApiUrl = $"https://pokeapi.co/api/v2/pokemon/{id}/";
+        var pokeResponse = await _httpClient.GetStringAsync(spriteApiUrl);
+        var pokeJson = JObject.Parse(pokeResponse);
+
+        string retroSprite =
+            pokeJson["sprites"]["versions"]["generation-v"]["black-white"]["front_default"]
+            ?.ToString() ?? "";
+
+        results.Add(new
+        {
+            name = name,
+            imageUrl = retroSprite
+        });
+    }
 
     return Json(results);
 }
 
-
-
-//Adivinar Pokémon
+//adivinar Pokémon
         [HttpPost]
         [Route("/Pokedex/Guess")]
         public IActionResult Guess(string guess)
